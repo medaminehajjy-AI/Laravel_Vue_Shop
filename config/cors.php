b2eefd035293b775c46996b517e7f289b7b7cb6b
medaminehajjy-AI <medaminehajjy@gmail.com>
2026-05-13 17:13:26 +0100

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://laravel-vue-shop-sehf.vercel.app',
+        '#^https://.*\.vercel\.app$#',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:8000',
