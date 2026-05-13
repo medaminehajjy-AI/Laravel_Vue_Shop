a135a7162ca371e3c8cb8faffeeddf0ea235b09a
medaminehajjy-AI <medaminehajjy@gmail.com>
2026-05-13 20:27:01 +0100

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -8,20 +8,13 @@ return [
 
     'allowed_origins' => [
         'https://laravel-vue-shop-sehf-89an8d9qm-medaminehajjy-ais-projects.vercel.app',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
     ],
 
-    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#',
+    ],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
