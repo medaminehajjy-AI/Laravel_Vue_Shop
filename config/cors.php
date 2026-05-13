f3f7307b5362f6692387c3fd68fd1600772f75d5
medaminehajjy-AI <medaminehajjy@gmail.com>
2026-05-13 22:03:04 +0100

    add placeholder png

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,7 @@ return [
 
     'allowed_origins' => [
         'https://laravel-vue-shop-sehf-89an8d9qm-medaminehajjy-ais-projects.vercel.app',
+        'http://localhost:5173',
     ],
 
     'allowed_origins_patterns' => [
